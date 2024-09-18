64c58fc67e3b5c2e9fc10ffad943a94ed287fac5
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-09-18 19:25:04 +0100

    Project setup

--- app/page.js
+++ app/page.js
@@ -1,10 +1,31 @@
-import Navigation from "./components/Navigation";
+import Image from "next/image";
+import Navigation from "./_components/Navigation";
+import Link from "next/link";
+import bg from "@/public/bg.png";
 
-export default function Home() {
+export default function Page() {
   return (
-    <div>
-      <Navigation />
-      <h1> The WIld Oasis. Welcome to paradise </h1>;
-    </div>
+    <main className="mt-24">
+      <Image
+        src={bg}
+        quality={80}
+        fill
+        placeholder="blur"
+        className="object-cover object-top"
+        alt="Mountains and forests with two cabins"
+      />
+
+      <div className="relative z-10 text-center">
+        <h1 className="text-8xl text-primary-50 mb-10 tracking-tight font-normal">
+          Welcome to paradise.
+        </h1>
+        <Link
+          href="/cabins"
+          className="bg-accent-500 px-8 py-6 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
+        >
+          Explore luxury cabins
+        </Link>
+      </div>
+    </main>
   );
 }
