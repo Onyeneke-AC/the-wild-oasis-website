f05aea8c7d2d63c3cfcfcd7e8708a9961bb29948
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-09-26 08:53:40 +0100

    implementing the reservation context

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -2,8 +2,9 @@ import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
 import Spinner from "../_components/Spinner";
 import Filter from "../_components/Filter";
+import ReservationReminder from "../_components/ReservationReminder";
 
-export const revalidate = 3600;
+// export const revalidate = 3600;
 
 export const metadata = {
   title: "Cabins",
@@ -31,6 +32,7 @@ export default function Page({ searchParams }) {
 
       <Suspense fallback={<Spinner />} key={filter}>
         <CabinList filter={filter} />
+        <ReservationReminder />
       </Suspense>
     </div>
   );
